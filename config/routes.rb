851e6c1b89b51bebc1ec2a5e8c73ff7b1b46d9c3
maxime-escamez@telecomnancy.net <maxime-escamez@telecomnancy.net>
2018-05-21 17:19:29 -0400

    More ruby-on-rails setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'  # tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
+
+  root 'welcome#index' # tells Rails to map requests to the root of the application to the welcome controller's index action
 end
